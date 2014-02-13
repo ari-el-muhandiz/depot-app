72a65b2abc14b71d22c749bc6a0c29aaa491d2c9
Ari Firmanto <arifirmanto@gmail.com>
2014-02-13 11:02:31 +0700

    implement internalization

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Depot::Application.routes.draw do
-  resources :orders
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  resources :users
 
-  resources :line_items
 
-  resources :carts
-
-  get "store/index"
   resources :products do
     get :who_bought, on: :member
   end
   
-  root 'store#index', as: 'store'
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
